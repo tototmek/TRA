cfa44821621e9159b6359614c04fed8a4952a889
Tomek Zebrowski <zebromek@gmail.com>
2023-12-14 15:17:54 +0100

    Implement minimal pass-trough audio block

--- delay.py
+++ delay.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python
 
 import jack
+import threading
 
-clientname = "delay"
-client = jack.Client(clientname)
-client.inports.register(f"signal_in")
-client.outports.register(f"signal_out")
-inport = client.inports[0]
-outport = client.outports[0]
+client = jack.Client("delay")
+client.inports.register("signal_in")
+client.outports.register("signal_out")
+exit_event = threading.Event()
 
 
 @client.set_process_callback
 def process(frames):
-    outport.get_buffer()[:] = inport.get_buffer()
-    pass
+    client.outports[0].get_buffer()[:] = client.inports[0].get_buffer()
+
+
+@client.set_shutdown_callback
+def shutdown(status, reason):
+    print("JACK shutdown!")
+    print("status:", status)
+    print("reason:", reason)
+    exit_event.set()
 
 
 with client:
+    print("Press Ctrl+C to stop")
     try:
-        while True:
-            pass
+        exit_event.wait()
     except KeyboardInterrupt:
-        print(" Terminated by user")
+        print("\nInterrupted by user")
