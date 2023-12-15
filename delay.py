61e74516ac6b616cf7b3bbb0d0812679b9b2440f
Tomek Zebrowski <zebromek@gmail.com>
2023-12-15 15:08:03 +0100

    Update diagram

--- delay.py
+++ delay.py
@@ -11,24 +11,25 @@ exit_event = threading.Event()
 
 # Proponowany układ:
 #
-#   input ----+--------------------------------------->(+)---->output
+#   input ----+------------------------5.Gain1-------->(+)---->output
 #             |                                         ᐱ
 #             |                                         |
 #             V                                         |
-#            (+)---> 1.Delay ----> 2.Filter ---+---> 3.Gain
+#            (+)---> 1.Delay ----> 2.Filter ---+---> 3.Gain2
 #             ᐱ                                |
 #             |                                |
 #             |                                |
-#             +----------- 4. Gain ------------+
+#             +----------- 4.Gain3 ------------+
 #
 # 1.Delay – parametr time – wprowadza do sygnału opóźnienie o pewną liczbę próbek
 # 2.Filter – parametry brzmienia – moim zdaniem najlepiej jakiś filtr noi,
 #            regulowane parametry, konieczne wyliczanie parametrów filtru
 #            w locie, być może podział na sekcje bikwadratowe czy coś takiego, idk
 #           kolejność delaya i filtru można dowolnie zamieniać
-# 3.Gain – parametr wet – kontroluje jak dużo opóźnionego sygnału idzie na output
-# 4.Gain – parametr feedback – kontroluje tłumienie każdego kolejnego powtórzenia
+# 3.Gain2 – parametr wet – kontroluje jak dużo opóźnionego sygnału idzie na output
+# 4.Gain3 – parametr feedback – kontroluje tłumienie każdego kolejnego powtórzenia
 #          danego fragmentu sygnału.
+# 5.Gain1 – parametr dry
 #                                           ᐱ
 #                                           |
 @client.set_process_callback  #             |
