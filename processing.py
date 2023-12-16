8d55aa5329da10ca3f14439b549dc143915bb193
Tomek Zebrowski <zebromek@gmail.com>
2023-12-16 12:07:35 +0100

    Enable dynamic parameter configuration

--- processing.py
+++ processing.py
@@ -1,22 +1,31 @@
 from shift_register import ShiftRegister
 
+MAX_DELAY_SAMPLES = 65536
+
 
 class GainBlock:
     def __init__(self, gain: float) -> None:
-        self.gain = gain
+        self.set_gain(gain)
 
     def process(self, x: float) -> float:
         return x * self.gain
 
+    def set_gain(self, gain: float) -> None:
+        self.gain = gain
+
 
 class DelayBlock:
     def __init__(self, time: float, samplerate: float) -> None:
-        self.delay = round(samplerate * time)
-        self.register = ShiftRegister(self.delay)
+        self.samplerate = samplerate
+        self.set_delay(time)
+        self.register = ShiftRegister(MAX_DELAY_SAMPLES)
 
     def process(self, x: float) -> float:
         self.register.shift(x)
         return self.register.at(self.delay - 1)
+
+    def set_delay(self, time: float) -> None:
+        self.delay = max(1, min(MAX_DELAY_SAMPLES, round(self.samplerate * time)))
 
 
 class FilterBlock:
