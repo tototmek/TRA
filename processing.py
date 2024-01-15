2b790d8b5c25f92fce7c16987ae9ccc9d8b69d61
Przemysław Wyziński <przemyslaw.wyzinski@gemius.com>
2024-01-15 13:19:02 +0100

    fixed filter block

--- processing.py
+++ processing.py
@@ -1,7 +1,7 @@
 from shift_register import ShiftRegister
 from params import DelayEffectParams, FilterParams
 from scipy import signal
-import numpy as np
+import time
 
 MAX_DELAY_SAMPLES = 65536
 SAMPLE_RATE = 44100
@@ -32,21 +32,50 @@ class DelayBlock:
         self.delay = max(1, min(MAX_DELAY_SAMPLES, round(self.samplerate * time)))
 
 
-# TODO: Implement the correct filter block
 class FilterBlock:
     def __init__(self, params: FilterParams) -> None:
         self.prev_x = 0
-        self.buffer = ShiftRegister(100)
+        self.buffer_x_1 = ShiftRegister(3)
+        self.buffer_x_2 = ShiftRegister(3)
+        self.buffer_x_3 = ShiftRegister(3)
+        self.buffer_x_4 = ShiftRegister(3)
+        self.buffer_y_1 = ShiftRegister(2)
+        self.buffer_y_2 = ShiftRegister(2)
+        self.buffer_y_3 = ShiftRegister(2)
+        self.buffer_y_4 = ShiftRegister(2)
         self.set_params(params)
         self.b, self.a = signal.butter(4, [self.low_freq, self.high_freq], fs=SAMPLE_RATE, btype="band")
         self.sos = signal.tf2sos(self.b, self.a)
+        print(self.sos)
 
     def process(self, x: float) -> float:
-        # self.buffer.shift(x)
-        # return self.filterness * self.buffer.register[1]
-        self.buffer.shift(x)
-        a = signal.sosfilt(self.sos, self.buffer.register)
-        return a[9]
+        self.buffer_x_1.shift(x)
+        y1 = self.sos[0][0]*self.buffer_x_1.at(0) + self.sos[0][1]*self.buffer_x_1.at(1) + self.sos[0][2]*self.buffer_x_1.at(2)
+        y1 = y1 - self.sos[0][4]*self.buffer_y_1.at(0) - self.sos[0][5]*self.buffer_y_1.at(1)
+        y1 = self.sos[0][3]*y1
+        self.buffer_y_1.shift(y1)
+
+        # return y1
+
+        self.buffer_x_2.shift(y1)
+        y2 = self.sos[1][0]*self.buffer_x_2.at(0) + self.sos[1][1]*self.buffer_x_2.at(1) + self.sos[1][2]*self.buffer_x_2.at(2)
+        y2 = y2 - self.sos[1][4]*self.buffer_y_2.at(0) - self.sos[1][5]*self.buffer_y_2.at(1)
+        y2 = self.sos[1][3]*y2
+        self.buffer_y_2.shift(y2)
+
+        self.buffer_x_3.shift(y2)
+        y3 = self.sos[2][0]*self.buffer_x_3.at(0) + self.sos[2][1]*self.buffer_x_3.at(1) + self.sos[2][2]*self.buffer_x_3.at(2)
+        y3 = y3 - self.sos[2][4]*self.buffer_y_3.at(0) - self.sos[2][5]*self.buffer_y_3.at(1)
+        y3 = self.sos[2][3]*y3
+        self.buffer_y_3.shift(y3)
+
+        self.buffer_x_4.shift(y3)
+        y4 = self.sos[3][0]*self.buffer_x_4.at(0) + self.sos[3][1]*self.buffer_x_4.at(1) + self.sos[3][2]*self.buffer_x_4.at(2)
+        y4 = y4 - self.sos[3][4]*self.buffer_y_4.at(0) - self.sos[3][5]*self.buffer_y_4.at(1)
+        y4 = self.sos[3][3]*y4
+        self.buffer_y_4.shift(y4)
+
+        return y4
 
     def set_params(self, params: FilterParams) -> None:
         self.low_freq = params.low_freq
